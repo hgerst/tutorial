34c084789b4588af09a26541c60b3cb7045a1c9f
hgerst <hayden.gerst@gmail.com>
2015-12-27 23:37:42 -0800

    Added +4 feature in js

--- index.js
+++ index.js
@@ -3,10 +3,27 @@
 (function() {
 	$(document).ready(function(){
 		$("h2").click(hideHeaders);
+		$(".iv").click(function() {
+			incrementIV(this, getIncrement());
+		});
 	});
 	
 	function hideHeaders() {
 		$("h2").hide();
 	}
+	
+	function getIncrement() {
+		return 4;
+	}
+	
+	function incrementIV(button, increment) {
+		var ivHolder = $(button).find(".iv-value");
+		var ivCount = parseInt(ivHolder.html());
+		ivHolder.html(ivCount + increment);
+		
+	}
+	
+	
+	
 	
 })();
